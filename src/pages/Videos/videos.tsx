916d30810da4a41b67bb2bcecf1c91aed41df8b7
felipe domingues <fdomingueskt@gmail.com>
2025-11-26 23:25:03 -0300

    fix all

--- src/pages/Videos/videos.tsx
+++ src/pages/Videos/videos.tsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import VideoCard from "../Videos/videoCard/videoCard";
-import "./videos.css";
+import "./Videos.css";
 import { apiService } from "../../services/api";
 
 type CmsVideo = {
