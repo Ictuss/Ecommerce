0da9d4e416cea9309870b90a85ce507b7e37e833
felipe domingues <fdomingueskt@gmail.com>
2025-11-26 23:15:44 -0300

    fix main

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css"; // Importe o index.css aqui
-import App from "./App.tsx";
-import { CartProvider } from "./contexts/CartContext.tsx";
+import App from "./App";
+import { CartProvider } from "./contexts/CartContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
